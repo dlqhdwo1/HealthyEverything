e0e06422e6e00fc3511f896efc21dff3f3a777d5
jungguelee <star5436@naver.com>
2025-12-15 07:46:23 +0900

    2025-12-14 프로젝트구성작업

--- HealthyEverything-api/src/test/java/com/example/healthyeverythingapi/HealthyEverythingApiApplicationTests.java
+++ HealthyEverything-api/src/test/java/com/example/healthyeverythingapi/HealthyEverythingApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.healthyeverythingapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//테스트
 @SpringBootTest
 class HealthyEverythingApiApplicationTests {
 
